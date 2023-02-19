582b3db43928c33ab737111502d737c850a5237e
vutiendat3601 <vutien.dat.3601@gmail.com>
2023-02-19 19:32:54 +0700

    edit Bid.java @Immutable comment

--- src/main/java/com/datvutech/jpwh/part2/_4_mappingclass/entity/Bid.java
+++ src/main/java/com/datvutech/jpwh/part2/_4_mappingclass/entity/Bid.java
@@ -11,7 +11,7 @@ import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = AccessLevel.PACKAGE)
-@Immutable // no public setter methods for any properties (all set in constructor)
+@Immutable // not be updated
 @Table(name = "_4_bids")
 @Entity
 public class Bid implements Serializable {
